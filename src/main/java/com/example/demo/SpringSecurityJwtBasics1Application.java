0eda086e5e399e33f334f90e894dbffed101b3d5
Rajji02 <rajat.agrawal@letsgettin.com>
2019-06-04 12:45:15 +0530

    rajat commiting

--- src/main/java/com/example/demo/SpringSecurityJwtBasics1Application.java
+++ src/main/java/com/example/demo/SpringSecurityJwtBasics1Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringSecurityJwtBasics1Application {
 
 	public static void main(String[] args) {
+		//its rajats commit bro 
 		SpringApplication.run(SpringSecurityJwtBasics1Application.class, args);
 	}
 
