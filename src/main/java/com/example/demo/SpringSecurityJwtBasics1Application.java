202ea69c23edd2f01a5000fd80a9e40c866d082c
Rajji02 <rajat.agrawal@letsgettin.com>
2019-06-04 12:46:42 +0530

    second by rajat

--- src/main/java/com/example/demo/SpringSecurityJwtBasics1Application.java
+++ src/main/java/com/example/demo/SpringSecurityJwtBasics1Application.java
@@ -8,6 +8,7 @@ public class SpringSecurityJwtBasics1Application {
 
 	public static void main(String[] args) {
 		//its rajats commit bro 
+		//my commit by rajat bro
 		SpringApplication.run(SpringSecurityJwtBasics1Application.class, args);
 	}
 
